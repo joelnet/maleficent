837ce890649ed37d0848ad0a2f3f755e644691d5
Joel Thoms <jthoms@gmail.com>
2019-03-13 01:17:57 -0700

    feat: 🎸 output dir should change on install

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const map = require("mojiscript/list/map");
 const fs = require("fs-extra")
 const { join } = require("path")
 
-const outfile = join(process.cwd(), "maleficent.json")
+const outfile = join(process.env.INIT_CWD || process.cwd(), "maleficent.json")
 
 const plugins = [
   { key: "env", plugin: require("./plugins/getEnvironmentVariables") },
